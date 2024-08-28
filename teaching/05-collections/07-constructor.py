ff8b29a65bd03aee5e59484c5a4a30ad5ee80eb9
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-08-28 10:01:24 +0100

    adds container types

---
+++ teaching/05-collections/07-constructor.py
@@ -0,0 +1,12 @@
+# empty tuple => ()
+# empty dict => {}
+# empty list => []
+
+print(tuple([1,2,3]))
+
+place_list = ["Belfast", "Yorkshire", "Wrexham", "London", "London", "Yorkshire"]
+unique_list = list(set(place_list))
+
+print(unique_list)
+print(type(unique_list))
+
---
+++ teaching/05-collections/08-membership.py
@@ -0,0 +1,10 @@
+fruits = ('apple', 'pear', 'orange', 'plum', 'apple')
+
+if 'orange' in fruits:
+  print("I'm not getting scurvy!")
+
+if 'orange' not in fruits:
+  print("I am getting scurvy!")
+
+for fruit in fruits:
+  print(fruit)
